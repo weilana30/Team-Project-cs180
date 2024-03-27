592605c2cb2ccb633d82e65b2d39dce46752da38
weilana30 <165208073+weilana30@users.noreply.github.com>
2024-03-27 17:11:58 -0400

    Update directMessage.java

--- directMessage.java
+++ directMessage.java
@@ -4,9 +4,13 @@ public class directMessage {
     private User [] users;
     private String messageFile;
 
+    //constructs a new Message Object
     public directMessage(User user1, User user2) {
         this.users = new User[]{user1, user2};
         this.messageFile = user1.getUsername() +"/"+ user2.getUsername() + "Message.txt";
+
+        //This is supposed to check if the file exists already, so it can't duplicate the chat
+        //It does not work though
         File file = new File(this.messageFile);
         File file2 = new File(user2.getUsername() +"/"+ user1.getUsername() + "Message.txt");
         if (file.exists()) {
@@ -16,6 +20,7 @@ public class directMessage {
             throw new IllegalArgumentException("These two users already have a chat." + messageFile);
         }
     }
+    //This adds a message to the end of the file message in the format user-who-sent-message:::message
     public boolean addMessage(String message, User user) {
         try {
             File
