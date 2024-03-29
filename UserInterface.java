c2c32cb6e41c0739ce7906a2ce493bf20120f045
RaishiVee <143196627+RaishiVee@users.noreply.github.com>
2024-03-29 16:23:00 -0400

    Create UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,23 @@
+public interface UserInterface {
+
+    String getName();
+
+    void setName(String name);
+
+    String getUsername();
+
+    void setUsername(String username);
+
+    String getPassword();
+
+    void setPassword(String password);
+
+    String getBirthday();
+
+    void setBirthday(String birthday);
+
+    String getEmail();
+
+    void setEmail(String email);
+
+}
