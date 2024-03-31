edcddf64ba2f82294323ba0614a74bbd413b27ba
RaishiVee <143196627+RaishiVee@users.noreply.github.com>
2024-03-31 16:24:26 -0400

    Update SignupInterface.java

--- SignupInterface.java
+++ SignupInterface.java
@@ -1,7 +1,9 @@
 public interface SignupInterface {
 
     boolean createUser(String username, String password);
+
     boolean checkUsername(String username);
+
     boolean checkPassword(String password);
 
 }
