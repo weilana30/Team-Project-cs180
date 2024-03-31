09bdc74a26e4155be09f09d7395495ae3b34152e
weilana30 <165208073+weilana30@users.noreply.github.com>
2024-03-31 18:05:34 -0400

    Update and rename directMessageInterface to directMessageInterface.java

---
+++ directMessageInterface.java
@@ -0,0 +1,16 @@
+import java.util.*;
+/**
+ * directMessageInterface
+ * <p>
+ * An interface for the directMessage class
+ *
+ * @author Andrew Weiland, lab section 15
+ * @version March 31, 2024
+ */
+public interface DirectMessageInterface {
+    User [] getUsers();
+    ArrayList<IndividualText> getMessages();
+    boolean addMessage(User user, String message);
+    boolean deleteMessage(IndividualText individualText, User user);
+    String toString();
+}
