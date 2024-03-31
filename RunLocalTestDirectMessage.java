cd5634aefe140fd7676605a9aad3cd51e5fd71b3
weilana30 <165208073+weilana30@users.noreply.github.com>
2024-03-31 18:07:21 -0400

    Update RunLocalTestDirectMessage.java

--- RunLocalTestDirectMessage.java
+++ RunLocalTestDirectMessage.java
@@ -5,7 +5,14 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.RunWith;
 import org.junit.runner.notification.Failure;
-
+/**
+ * RunLocalTestDirectMessage
+ * <p>
+ * Runs a test on the directMessage class and its methods
+ *
+ * @author Andrew Weiland, lab section 15
+ * @version March 31, 2024
+ */
 @RunWith(Enclosed.class)
 public class RunLocalTestDirectMessage {
     public static void main(String[] args) {
@@ -18,12 +25,20 @@ public class RunLocalTestDirectMessage {
             }
         }
     }
+    /**
+     * TestCase
+     * <p>
+     * Has several tests for the methods in DirectMessage
+     *
+     * @author Andrew Weiland, lab section 15
+     * @version March 31, 2024
+     */
     public static class TestCase {
         @Test(timeout = 1000)
-        public void CreateNewMessageTest() {
+        public void createNewMessageTest() {
             User user1 = new User("AndyMan_18", "MonkeyBananas6");
             User user2 = new User("The-Salty-Salzmann46", "LetsGoBoilers!@");
-            directMessage message = new directMessage(user1, user2);
+            DirectMessage message = new DirectMessage(user1, user2);
             User[] users = message.getUsers();
             Assert.assertArrayEquals("Make sure the user array is correct.", users, new User[]{user1, user2});
         }
@@ -32,28 +47,31 @@ public class RunLocalTestDirectMessage {
         public void addMessageTest() {
             User user1 = new User("AndyMan_18", "MonkeyBananas6");
             User user2 = new User("The-Salty-Salzmann46", "LetsGoBoilers!@");
-            directMessage message = new directMessage(user1, user2);
+            DirectMessage message = new DirectMessage(user1, user2);
             String textMessage = "Hello Andrew, how was you day today?";
             boolean adds = message.addMessage(user2, textMessage);
             Assert.assertTrue("Ensure that it the addMessage method does not fail.", adds);
-            Assert.assertEquals("Ensure that when you add a message it is being added correctly", user2.getUsername() + ":   " + textMessage + "\n", message.toString());
+            Assert.assertEquals("Ensure that when you add a message it is being added correctly",
+                    user2.getUsername() + ":   " + textMessage + "\n", message.toString());
             String textMessage2 = "It was excellent. I went to the mall with my mom!!";
             message.addMessage(user1, textMessage2);
             Assert.assertEquals("Ensure that it adds a new message correctly even if you already have a message in it.",
-                    user2.getUsername() + ":   " + textMessage + "\n" + user1.getUsername() + ":   " + textMessage2 + "\n", message.toString());
+                    user2.getUsername() + ":   " + textMessage + "\n" + user1.getUsername() + ":   "
+                            + textMessage2 + "\n", message.toString());
         }
 
         @Test(timeout = 1000)
         public void deleteMessageTest() {
             User user1 = new User( "AndyMan_18", "MonkeyBananas6");
             User user2 = new User("The-Salty-Salzmann46", "LetsGoBoilers!@");
-            directMessage message = new directMessage(user1, user2);
+            DirectMessage message = new DirectMessage(user1, user2);
             String textMessage = "Hello Andrew, how was you day today?";
             message.addMessage(user2, textMessage);
             IndividualText messageToDelete = message.getMessages().get(0);
             boolean removeFalse = message.deleteMessage(messageToDelete, user1);
             boolean removeTrue = message.deleteMessage(messageToDelete, user2);
-            Assert.assertFalse("Ensure that the message is not deleted if the wrong user tries to delete it.", removeFalse);
+            Assert.assertFalse("Ensure that the message is not deleted if the wrong user tries to delete it.",
+                    removeFalse);
             Assert.assertTrue("Ensure that the message is properly deleted", removeTrue);
             Assert.assertEquals("Ensure the message is properly deleted.", "", message.toString());
         }
@@ -61,7 +79,7 @@ public class RunLocalTestDirectMessage {
         public void updateFileTest() {
             User user1 = new User("AndyMan_18", "MonkeyBananas6");
             User user2 = new User("The-Salty-Salzmann46", "LetsGoBoilers!@");
-            directMessage message = new directMessage(user1, user2);
+            DirectMessage message = new DirectMessage(user1, user2);
             String textMessage = "Hello Andrew, how was you day today?";
             boolean adds = message.addMessage(user2, textMessage);
             String textMessage2 = "It was excellent. I went to the mall with my mom!!";
@@ -72,4 +90,5 @@ public class RunLocalTestDirectMessage {
         }
     }
 }
+
 
