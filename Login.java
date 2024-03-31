c326deff16a77b5a4ac8fec7e047ddc27963fafb
RaishiVee <143196627+RaishiVee@users.noreply.github.com>
2024-03-31 18:13:49 -0400

    Added comments

--- Login.java
+++ Login.java
@@ -2,12 +2,21 @@ public class Login implements LoginInterface {
 
     private User[] users;
 
+    // constructor for Login.java which takes in one parameter (User[] users)
+    // and instantiates it as long as it is not null
     public Login(User[] users) {
+        if (users == null) {
+            users = new User[10];
+        }
         this.users = users;
     }
 
+    // this method ensures that the username and password inputted match the
+    // username and password that is in the users array
     public boolean authenticate(String username, String password) {
 
+        // this iterates through users array and ensures that both inputs are in the users array as a user
+        // returns true if successful
         for (User user : users) {
             if (user != null) {
                 if (user.getUsername().equalsIgnoreCase(username) & user.getPassword().equals(password)) {
@@ -16,6 +25,7 @@ public class Login implements LoginInterface {
             }
         }
 
+        // otherwise returns false
         return false;
 
     }
