786ea42bfe5f2ead814b31dba93898fb864d161e
weilana30 <165208073+weilana30@users.noreply.github.com>
2024-03-31 18:05:58 -0400

    Update and rename IndividualTextInterface to IndividualTextInterface.java

--- IndividualTextInterface.java
+++ IndividualTextInterface.java
@@ -1,3 +1,11 @@
+/**
+ *IndividualTextInterface
+ * <p>
+ * An interface for the IndividualText class
+ *
+ * @author Andrew Weiland, lab section 15
+ * @version March 31, 2024
+ */
 public interface IndividualTextInterface {
     User getUser();
     String getText();
