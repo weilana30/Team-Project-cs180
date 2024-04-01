63ff2d8425031ab6476b60a7469a9c2aa84dfc22
ricardoliu0 <165210167+ricardoliu0@users.noreply.github.com>
2024-04-01 10:23:46 -0400

    Update SearchInterface.java

--- SearchInterface.java
+++ SearchInterface.java
@@ -1,3 +1,12 @@
+/**
+ * search
+ *
+ * <p>
+ * Create a way to find a user
+ *
+ * @author Ricardo Liu, lab section 15
+ * @version March 31, 2024
+ */
 public interface ProfileSearchInterface {
     void search();
 }
