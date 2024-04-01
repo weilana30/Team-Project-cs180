cd62c7a0e902b2d156f70ab768fca7e47bf67b1e
ricardoliu0 <165210167+ricardoliu0@users.noreply.github.com>
2024-04-01 10:23:32 -0400

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
+/**
+ * search
+ *
+ * <p>
+ * Create a way to find a user
+ *
+ * @author Ricardo Liu, lab section 15
+ * @version March 31, 2024
+ */
 public class ProfileSearch {
     private Profile profile;
     private ProfileViewerInterface profileViewer;
