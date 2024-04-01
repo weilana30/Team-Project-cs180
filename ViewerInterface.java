ad45886a6f55b87589b95655ca8855d6175adbb4
ricardoliu0 <165210167+ricardoliu0@users.noreply.github.com>
2024-04-01 10:24:50 -0400

    Update ViewerInterface.java

--- ViewerInterface.java
+++ ViewerInterface.java
@@ -1,3 +1,12 @@
+/**
+ * viewerinterface
+ *
+ * <p>
+ * Create a way to find a user
+ *
+ * @author Ricardo Liu, lab section 15
+ * @version March 31, 2024
+ */
 public interface ProfileViewerInterface {
     String displayUserInformation(String username);
 }
