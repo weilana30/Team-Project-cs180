b411ce288e7946d92af6239471893ecd87d957ec
RaishiVee <143196627+RaishiVee@users.noreply.github.com>
2024-04-13 12:28:48 -0400

    Added readFile method

--- Profile.java
+++ Profile.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 
 /**
@@ -13,7 +14,28 @@ public class Profile {
     private ArrayList<User> users;
 
     public Profile() {
-        this.users = new ArrayList<>();
+        boolean usersAdded = readFile(new File("Users.txt"));
+        if (!usersAdded) {
+            this.users = new ArrayList<>();
+        }
+    }
+
+    public boolean readFile(File usersFile) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(usersFile))) {
+
+            String line = bfr.readLine();
+
+            while (line != null) {
+                String[] userInfo = line.split(" ");
+                User user = new User(userInfo[0], userInfo[1], userInfo[2], userInfo[3], userInfo[4]);
+                users.add(user);
+                line = bfr.readLine();
+            }
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
     public void addUser(User user) {
         users.add(user);
@@ -65,22 +87,5 @@ public class Profile {
         } else {
             return null;
         }
-    }
-    public User getUserByPhoneNumber(String phoneNumber) {
-        for (User user : users) {
-            if (user.getPhoneNumber().equals(phoneNumber)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    public User getUserByEmail(String email) {
-        for (User user : users) {
-            if (user.getEmail().equals(email)) {
-                return user;
-            }
-        }
-        return null;
     }
 }
