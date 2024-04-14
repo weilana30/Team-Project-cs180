d7a3210afa531e7f735c9ce47993d17fc3c93ed5
ricardoliu0 <165210167+ricardoliu0@users.noreply.github.com>
2024-04-14 18:54:10 -0400

    Update ProfileInterface.java

--- ProfileInterface.java
+++ ProfileInterface.java
@@ -1,6 +1,6 @@
 
 /**
- * profileinterface
+ * profileInterface
  *
  * <p>
  * Creates an object that restores the information of a user
@@ -9,14 +9,14 @@
  * @version March 31, 2024
  */
 public interface ProfileInterface {
-    boolean readFile(File usersFile);
     void addUser(User user);
     User getUserByUsername(String username);
-    User getUserByPhoneNumber(String phoneNumber);
-    User getUserByEmail(String email);
     String getEmailByUsername(String username);
     String getPhoneNumberByUsername(String username);
     String getBirthdayByUsername(String username);
     String getPasswordByUsername(String username);
     String getNameByUsername(String username);
+    User getUserByPhoneNumber(String phoneNumber);
+    User getUserByEmail(String email);
+        
 }
