a9e53c2cb44de39163cfa03aff3a9969b3982b61
shree-v-b <165209088+shree-v-b@users.noreply.github.com>
2024-04-14 18:15:06 -0400

    Update FriendsInterface.java

--- FriendsInterface.java
+++ FriendsInterface.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public interface FriendsInterface {
+
     boolean addFriend(String friendUsername, String userUsername);
+
     boolean removeFriend(String friendUsername);
+
     boolean blockUser(String userToBlockUsername, String currentUserUsername);
+
+    boolean unblockUser(String userToUnblockUsername, String currentUserUsername);
+
+    ArrayList<User> getFriends(String username);
+
+    ArrayList<User> getBlocked(String username);
+
 }
