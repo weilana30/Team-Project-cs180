ed4d1ac16484261815c8c74e51bd18fdc3e0392a
ricardoliu0 <165210167+ricardoliu0@users.noreply.github.com>
2024-04-15 14:06:53 -0400

    Update ProfileViewer.java

--- ProfileViewer.java
+++ ProfileViewer.java
@@ -9,20 +9,44 @@
  */
 public class ProfileViewer implements ProfileViewerInterface {
     private Profile profile;
+
     public ProfileViewer(Profile profile) {
         this.profile = profile;
     }
-    public String displayUserInformation(String username) {
-        StringBuilder userInfo = new StringBuilder();
+
+    public String displayUserInformationByUsername(String username) {
         User user = profile.getUserByUsername(username);
         if (user != null) {
-            userInfo.append("Username: ").append(user.getUsername()).append("\n");
-            userInfo.append("Email: ").append(user.getEmail()).append("\n");
-            userInfo.append("Phone Number: ").append(user.getPhoneNumber()).append("\n");
-            userInfo.append("Birthday: ").append(user.getBirthday()).append("\n");
+            return displayUserInformation(user);
         } else {
-            userInfo.append("User not found!");
+            return "User not found!";
         }
+    }
+
+    public String displayUserInformationByPhoneNumber(String phoneNumber) {
+        User user = profile.getUserByPhoneNumber(phoneNumber);
+        if (user != null) {
+            return displayUserInformation(user);
+        } else {
+            return "User not found!";
+        }
+    }
+
+    public String displayUserInformationByEmail(String email) {
+        User user = profile.getUserByEmail(email);
+        if (user != null) {
+            return displayUserInformation(user);
+        } else {
+            return "User not found!";
+        }
+    }
+
+    public String displayUserInformation(User user) {
+        StringBuilder userInfo = new StringBuilder();
+        userInfo.append("Username: ").append(user.getUsername()).append("\n");
+        userInfo.append("Email: ").append(user.getEmail()).append("\n");
+        userInfo.append("Phone Number: ").append(user.getPhoneNumber()).append("\n");
+        userInfo.append("Birthday: ").append(user.getBirthday()).append("\n");
         return userInfo.toString();
     }
 }
