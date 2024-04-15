090f6ab317d9003e18b230430d4542ccfe430e2a
weilana30 <165208073+weilana30@users.noreply.github.com>
2024-04-15 13:39:38 -0400

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public interface ServerInterface {
 
-    void main(String[] args);
-
     void run();
 
-    void returningUser(String username, BufferedReader br, PrintWriter pw);
+    void returningUser(String username, BufferedReader br, PrintWriter pw) throws IOException;
 
-    void handleFriends(User user, BufferedReader bfr, PrintWriter pw);
+    void handleFriends(User user, BufferedReader bfr, PrintWriter pw) throws IOException;
 
-    void handleProfileSearch(BufferedReader br, PrintWriter pw);
+    void handleProfileSearch(BufferedReader br, PrintWriter pw) throws IOException;
 }
