63e318a79a34ea11928e9205aab4a081b39a7150
shree-v-b <165209088+shree-v-b@users.noreply.github.com>
2024-04-15 00:45:31 -0400

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,12 +1,15 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public interface ServerInterface {
-    
+
     void main(String[] args);
-    
+
     void run();
-    
+
     void returningUser(String username, BufferedReader br, PrintWriter pw);
-    
+
     void handleFriends(User user, BufferedReader bfr, PrintWriter pw);
-    
+
     void handleProfileSearch(BufferedReader br, PrintWriter pw);
 }
