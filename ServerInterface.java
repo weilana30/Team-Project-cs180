4e39ec783505a630513b2d9715d3036c0ea69270
ricardoliu0 <165210167+ricardoliu0@users.noreply.github.com>
2024-04-15 14:29:46 -0400

    Update ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -1,7 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+/**
+ * ServerInterface
+ * <p>
+ * Interface for Server
+ *
+ * @author Andrew Weiland, lab section 15
+ * @version April 15, 2024
+ */
 public interface ServerInterface {
 
     void run();
