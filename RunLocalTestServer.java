ace8856976f4aee497cbc064ebd3f603593ddb43
shree-v-b <165209088+shree-v-b@users.noreply.github.com>
2024-04-14 22:58:26 -0400

    Create RunLocalTestServer.java

---
+++ RunLocalTestServer.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(JUnit4.class)
+public class RunLocalTestServer {
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(RunLocalTestServer.class);
+        System.out.printf("Test Count: %d.\n", result.getRunCount());
+        if (result.wasSuccessful()) {
+            System.out.printf("Excellent - all local tests ran successfully.\n");
+        } else {
+            System.out.printf("Tests failed: %d.\n", result.getFailureCount());
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+    }
+  
+    @Test
+    public void testConstructorWithClientSocket() {
+        Socket testSocket = new Socket();
+      
+        Server server = new Server(testSocket);
+        assertNotNull(server);
+      
+        assertEquals(testSocket, server.clientSocket);
+    }
+}
